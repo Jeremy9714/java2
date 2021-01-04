1364853b27d307e054b2f7d8face3afe641255df
CYZ <1822550890@qq.com>
2021-01-04 15:46:01 +0000

    匿名子类创建对象

--- JavaSenior/src/day01/ThreadTest.java
+++ JavaSenior/src/day01/ThreadTest.java
@@ -9,7 +9,18 @@ public class ThreadTest {
         //创建Thread类的子类的对象
         MyThread myThread = new MyThread();
         //通过此对象调用start()方法
-        myThread.start();
+        myThread.start();//启动当前线程；调用当前线程的run()方法
+
+        //重新创建一个线程的对象
+        MyThread myThread1 = new MyThread();
+        myThread1.start();
+
+        //如下操作依然在main线程中运行
+        for (int i = 0; i < 100; ++i) {
+            if (i % 2 != 0) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+        }
 
     }
 }
@@ -19,9 +30,9 @@ class MyThread extends Thread {
     //重写run()方法
     @Override
     public void run() {
-        for(int i=0;i<100;++i){
-            if (i%2==0){
-                System.out.println(i);
+        for (int i = 0; i < 100; ++i) {
+            if (i % 2 == 0) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
             }
         }
     }
