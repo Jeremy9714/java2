cdd8525e21fbb5b9f587cc58bdcd6f382338e666
CYZ <1822550890@qq.com>
2021-01-04 22:24:05 +0000

    同步代码块解决线程安全问题(继承和实现)

--- JavaSenior/day02/src/test1/WindowTest2.java
+++ JavaSenior/day02/src/test1/WindowTest2.java
@@ -1,20 +1,34 @@
 package test1;
 
 /**
+ * 创建三个窗口卖票，，总票数为100张，使用实现Runnable接口的方式
+ * 说明: 可以考虑使用this充当同步监视器
  * @author Chenyang
  * @create 2021-01-04-18:46
  */
+//class Dog{}
 
 class Window1 implements Runnable {
     private int ticket = 100;
+    //同步监视器可以是任何一个类的对象; 多个线程必须要共用同一把锁
+    //Dog dog  = new Dog();
 
     @Override
     public void run() {
         while (true) {
-            if (ticket > 0) {
-                System.out.println(Thread.currentThread().getName() + "卖票，票号为" + ticket);
-                ticket--;
-            } else break;
+            synchronized (this) {//同步代码块
+                if (ticket > 0) {
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(Thread.currentThread().getName() + "卖票，票号为" + ticket);
+                    ticket--;
+                } else break;
+            }
         }
     }
 }
--- JavaSenior/day02/src/test1/WindowsTest.java
+++ JavaSenior/day02/src/test1/WindowsTest.java
@@ -1,21 +1,33 @@
 package test1;
 
 /**
- * 创建三个窗口卖票，总票数为一百张
- *
+ * 创建三个窗口卖票，总票数为100张，使用继承Thread类的方式
+ * 说明: 可以考虑使用当前类充当同步监视器(类名.class是一个对象)
  * @author Chenyang
  * @create 2021-01-04-18:09
  */
 class Window extends Thread {
     private static int ticket = 100;
+    //静态对象作为锁
+    //private static final Object obj = new Object();
 
     @Override
     public void run() {
         while (true) {
-            if (ticket > 0) {
-                System.out.println(getName() + ": 卖票，票号为" + ticket);
-                ticket--;
-            }else break;
+//            synchronized (obj) {
+            synchronized (Window.class){ //Class cla = Window.class; Window.class只会加载一次
+                if (ticket > 0) {
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(getName() + ": 卖票，票号为" + ticket);
+                    ticket--;
+                } else break;
+            }
         }
     }
 }
