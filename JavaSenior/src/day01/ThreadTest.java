2ebbfd56a418f109fb475da6f181e61e4ab8c7dd
CYZ <1822550890@qq.com>
2021-01-04 12:53:22 +0000

    第一个线程

--- JavaSenior/src/day01/ThreadTest.java
+++ JavaSenior/src/day01/ThreadTest.java
@@ -5,6 +5,24 @@ package day01;
  * @create 2021-01-04-11:12
  */
 public class ThreadTest {
+    public static void main(String[] args) {
+        //创建Thread类的子类的对象
+        MyThread myThread = new MyThread();
+        //通过此对象调用start()方法
+        myThread.start();
 
+    }
+}
 
+//创建一个继承于Thread类的子类
+class MyThread extends Thread {
+    //重写run()方法
+    @Override
+    public void run() {
+        for(int i=0;i<100;++i){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
 }
