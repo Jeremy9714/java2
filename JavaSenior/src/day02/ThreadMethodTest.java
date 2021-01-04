579147d4ed431b4374cf4d85c5f43951c6b5cd0d
CYZ <1822550890@qq.com>
2021-01-04 16:43:41 +0000

    yield()、join()、sleep()、isAlive()方法

---
+++ JavaSenior/src/day02/ThreadMethodTest.java
@@ -0,0 +1,53 @@
+package day02;
+
+/**
+ * Thread中的常用方法
+ *
+ * @author Chenyang
+ * @create 2021-01-04-15:50
+ */
+class HelloThread extends Thread {
+    public HelloThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                try {
+                    sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+//            if(i%20==0){
+//                Thread.yield();
+//            }
+        }
+    }
+}
+
+public class ThreadMethodTest {
+    public static void main(String[] args) {
+        //线程改名
+        Thread.currentThread().setName("主线程");
+        //通过构造器给线程命名
+        HelloThread thread = new HelloThread("线程一");
+        thread.start();
+
+        for (int i = 0; i < 100; ++i) {
+            System.out.println(Thread.currentThread().getName() + ":" + i);
+            if (i == 20) {
+                try {
+                    thread.join();//
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println(thread.isAlive());
+    }
+}
