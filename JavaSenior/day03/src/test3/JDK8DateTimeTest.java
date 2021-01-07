4b4aff061d0ecb43fc96f0eac0d497c518c9d69b
CYZ <1822550890@qq.com>
2021-01-07 14:00:16 +0000

    Instant类的使用

--- JavaSenior/day03/src/test3/JDK8DateTimeTest.java
+++ JavaSenior/day03/src/test3/JDK8DateTimeTest.java
@@ -2,17 +2,40 @@ package test3;
 
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 
 /**
- * LocalDate、LocalTime、LocalDateTime的使用
+ * JDK 8的新日期时间API
  * @author Chenyang
  * @create 2021-01-07-12:21
  */
 public class JDK8DateTimeTest {
+    /**
+     * Instant的使用
+     * 类似于java.util.Date类的使用
+     */
+    @Test
+    public void test1(){
+        //now(): 获取本初子午线对应的标准时间
+        Instant instant = Instant.now();
+        System.out.println(instant);
 
+        //添加时间的偏移量
+        OffsetDateTime offsetDateTime = instant.atOffset(ZoneOffset.ofHours(8));
+        System.out.println(offsetDateTime);
+
+        //toEpochMilli(): 获取自1970年1月1日0时0分0秒(UTC)开始的毫秒数
+        long milli = instant.toEpochMilli();
+        System.out.println(milli);
+
+        //ofEpochMilli(): 通过给定的毫秒数，获取Instant实例
+        Instant instant1 = Instant.ofEpochMilli(milli);
+        System.out.println(instant1);
+    }
+
+    /**
+     * LocalDate、LocalTime、LocalDateTime类的使用
+     */
     @Test
     public void test(){
         //now(): 获取当前的日期、时间、日期+时间
