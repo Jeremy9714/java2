009f45c131cf87323df296607b140238d54931aa
CYZ <1822550890@qq.com>
2021-01-09 17:50:05 +0000

    TreeSet的使用(自然排序和定制排序)

--- JavaSenior/day04/src/test4/TreeSetTest.java
+++ JavaSenior/day04/src/test4/TreeSetTest.java
@@ -2,13 +2,76 @@ package test4;
 
 import org.junit.Test;
 
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
 /**
+ * TreeSet的使用
+ *
  * @author Chenyang
  * @create 2021-01-09-15:39
  */
 public class TreeSetTest {
     @Test
-    public void test(){
+    public void test() {
+        TreeSet set = new TreeSet();
 
+        //不能添加不同类的对象
+//        set.add(123);
+//        set.add(123);
+//        set.add("AA");
+//        set.add(new User(12, "tome"));
+
+        //举例一
+//        set.add(123);
+//        set.add(456);
+//        set.add(789);
+//        set.add(0);
+
+        set.add(new User(12, "Tom"));
+        set.add(new User(18, "Jeremy"));
+        set.add(new User(10, "Sean"));
+        set.add(new User(15, "Jeck"));
+        set.add(new User(13, "Jason"));
+        set.add(new User(40, "Jason"));
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void test1() {
+        Comparator com = new Comparator() {
+            @Override
+            //按年龄从小到大排序
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof User && o2 instanceof User) {
+                    User u1 = (User) o1;
+                    User u2 = (User) o2;
+                    return Integer.compare(u1.getAge(), u2.getAge());
+                } else throw new RuntimeException("类型不匹配");
+            }
+        };
+
+        //定制排序
+        TreeSet set = new TreeSet(com);
+        set.add(new User(12, "Tom"));
+        set.add(new User(18, "Jeremy"));
+        set.add(new User(10, "Sean"));
+        set.add(new User(15, "Jeck"));
+        set.add(new User(13, "Jason"));
+        set.add(new User(13, "Peter"));
+        set.add(new User(40, "Jason"));
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println("******************************");
+        //Java8新特性: Lambda表达式
+        set.forEach(System.out::println);
     }
 }
--- JavaSenior/day04/src/test4/User.java
+++ JavaSenior/day04/src/test4/User.java
@@ -6,7 +6,7 @@ import java.util.Objects;
  * @author Chenyang
  * @create 2021-01-09-12:32
  */
-public class User {
+public class User implements Comparable {
     private int age;
     private String name;
 
@@ -51,5 +51,21 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(age, name);
+    }
+
+    @Override
+    //自然排序
+    //按照姓名从大到小排序，年龄从小到大排序
+    public int compareTo(Object o) {
+        if (o instanceof User) {
+            User user = (User) o;
+            int res = -this.name.compareTo(user.name);
+            if (res != 0) {
+                return res;
+            } else {
+                return Integer.compare(this.age, user.age);
+            }
+
+        } else throw new RuntimeException("类型不匹配");
     }
 }
