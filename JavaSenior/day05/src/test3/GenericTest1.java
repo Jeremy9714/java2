3b79388b5813ae79aa4dac1aca75586d458687a9
CYZ <1822550890@qq.com>
2021-01-10 22:18:37 +0000

    使用通配符后数据的读取和写入要求

--- JavaSenior/day05/src/test3/GenericTest1.java
+++ JavaSenior/day05/src/test3/GenericTest1.java
@@ -2,6 +2,7 @@ package test3;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,11 +20,29 @@ public class GenericTest1 {
         List<Object> list1 = null;
         List<String> list2 = null;
 
+        //通配符?: 公共父类
         List<?> list = null;
         list = list1;
         list = list2;
-        print(list1);
-        print(list2);
+        //编译通过
+//        print(list1);
+//        print(list2);
+
+        ArrayList<String> list3 = new ArrayList<>();
+        list3.add("AA");
+        list3.add("BB");
+        list3.add("CC");
+        list = list3;
+
+        //添加(写): 对于List<?>就不能向其内部添加数据
+        //除了添加null以外
+//        list.add("AA");
+        list.add(null);
+
+        //获取(读): 允许读取数据，读取的数据类型为Object
+        Object o = list.get(0);
+        System.out.println(o);
+
     }
 
     public void print(List<?> list){
