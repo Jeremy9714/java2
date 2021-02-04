b7c95445eef6ac0168146eb2ab9794481fc2f011
CYZ <1822550890@qq.com>
2021-02-04 11:54:46 +0000

    debug

--- JavaSenior/day01/src/day02/ThreadDemo.java
+++ JavaSenior/day01/src/day02/ThreadDemo.java
@@ -23,7 +23,7 @@ public class ThreadDemo {
             @Override
             public void run() {
                 for (int i = 0; i < 100; ++i) {
-                    if (i % 2 == 0) {
+                    if (i % 2 != 0) {
                         System.out.println(Thread.currentThread().getName() + ":" + i);
                     }
                 }
