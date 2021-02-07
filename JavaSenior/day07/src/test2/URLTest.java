b910bd4fd9afa7545f9d9a08ba762f359c258636
CYZ <1822550890@qq.com>
2021-02-07 20:59:12 +0000

    补充

--- JavaSenior/day07/src/test2/URLTest.java
+++ JavaSenior/day07/src/test2/URLTest.java
@@ -13,20 +13,27 @@ public class URLTest {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://localhost:8080/examples/hello.txt?username=Jeremy&password=123");
+            //URL url = new URL("https","www.website.com",2020,"/home/exm.txt");
 
             //getProtocol(): 获取该URL的协议名
             System.out.println(url.getProtocol());
+
             //getHost(): 获取该URL的主机名
             System.out.println(url.getHost());
+
             //getPort(): 获取该URL的端口号
             System.out.println(url.getPort());
+
             //getPath(): 获取该URL的文件路径
             System.out.println(url.getPath());
+
             //getQuery(): 获取该URL的查询名
             System.out.println(url.getQuery());
+
             //getFile(): 获取该URL的文件名
             System.out.println(url.getFile());
 
+            //toString(): 完整的URL地址
             System.out.println(url);
         } catch (MalformedURLException e) {
             e.printStackTrace();
