86b09fe488714c68b41d6b7435671117c155c5ac
CYZ <1822550890@qq.com>
2021-02-09 10:21:09 +0000

    补充供给型接口与函数型接口的使用

--- JavaSenior/day09/src/test2/LambdaTest2.java
+++ JavaSenior/day09/src/test2/LambdaTest2.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
  * Java内置的四大核心函数式接口
@@ -35,11 +37,48 @@ public class LambdaTest2 {
     }
 
     /*
+    供给型接口的使用
+     */
+
+    @Test
+    public void test2() {
+        List<Integer> list = getIntegerList(10, () -> (int) (Math.random() * 10));
+        list.forEach(System.out::println);
+    }
+
+    public List<Integer> getIntegerList(int num, Supplier<Integer> sup) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < num; ++i) {
+            list.add(sup.get());
+        }
+        return list;
+    }
+
+    /*
+    函数型接口的使用
+     */
+    @Test
+    public void test3() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        List<Double> doubles = intToDouble(list, integer -> (double) integer);
+        doubles.forEach(System.out::println);
+    }
+
+    public List<Double> intToDouble(List<Integer> list, Function<Integer, Double> func) {
+        List<Double> returnList = new ArrayList<>();
+        for (Integer integer : list) {
+            returnList.add(func.apply(integer));
+        }
+        return returnList;
+    }
+
+
+    /*
     断定型接口的使用
      */
     @Test
-    public void test2() {
-        List<String> list = Arrays.asList("南京","北京","天津","东京");
+    public void test4() {
+        List<String> list = Arrays.asList("南京", "北京", "天津", "东京");
 //        List<String> filterList = filterString(list, new Predicate<String>() {
 //            @Override
 //            public boolean test(String s) {
