6c64de078c360227042ef9d80709be2e986a30db
CYZ <1822550890@qq.com>
2021-02-09 16:03:29 +0000

    Optional类应用实例

--- JavaSenior/day09/src/test5/OptionalTest.java
+++ JavaSenior/day09/src/test5/OptionalTest.java
@@ -19,9 +19,12 @@ public class OptionalTest {
 //        girl = null;
         Optional<Girl> girl = Optional.of(g1);
         System.out.println(girl);
+        System.out.println();
+
         //Optional.empty(): 创建一个空的Optional实例
         Optional<Object> empty = Optional.empty();
         System.out.println(empty);
+        System.out.println();
 
         Girl g2 = new Girl();
         g2 = null;
@@ -37,5 +40,36 @@ public class OptionalTest {
             target = girl2.orElse(new Girl("Lilly"));
         }
         System.out.println(target);
+        System.out.println();
+
+        Girl g3 = null;
+        Optional<Girl> girl3 = Optional.ofNullable(g3);
+        if (girl3.isPresent()){
+            target = girl3.get();
+        }else{
+            //T orElseGet(Supplier<? extends T> sup): 如果有值将其返回，否则返回由供给型接口实现提供的对象
+            target = girl3.orElseGet(Girl::new);
+        }
+        System.out.println(target);
     }
+
+    //Optional类应用实例
+    public String getGirlName(Boy boy){
+        Optional<Boy> ob = Optional.ofNullable(boy);
+        Boy newBoy = ob.orElse(new Boy(new Girl("生成的男友的女友")));
+
+        Girl girl = newBoy.getGirl();
+        Optional<Girl> og = Optional.ofNullable(girl);
+        Girl newGirl = og.orElse(new Girl("生成的女友"));
+        return newGirl.getName();
+    }
+
+    @Test
+    public void test2(){
+        //Boy boy = null;
+        Boy boy = new Boy();
+        String girlName = getGirlName(boy);
+        System.out.println(girlName);
+    }
+
 }
